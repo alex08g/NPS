ac5e3ac4b53f94448d81716e43d087d4f81b7460
alex08g <allex.araujo088@gmail.com>
2021-02-24 22:41:43 -0300

    feat: criando arquivo de rotas

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata'
 import './database/connection'
 import express from 'express'
 
+import routes from './routes'
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hello' })
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => console.log('Server is running! 🚀'))
